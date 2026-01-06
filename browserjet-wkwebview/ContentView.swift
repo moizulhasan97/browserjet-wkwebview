eaa986b435f51ef73deff6f39ab5c6785e193962
Moiz Ul Hasan <moiz.hasan@ext.beyond.one>
2026-01-06 23:14:48 +0500

    add initial files || gitignore

---
+++ browserjet-wkwebview/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  browserjet-wkwebview
+//
+//  Created by Moiz Ul Hasan on 06/01/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ browserjet-wkwebview/browserjet_wkwebviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  browserjet_wkwebviewApp.swift
+//  browserjet-wkwebview
+//
+//  Created by Moiz Ul Hasan on 06/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct browserjet_wkwebviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
