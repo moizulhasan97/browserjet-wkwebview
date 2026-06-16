577848668664b6d85272e7f0edf61eb04cc7b470
Moiz Ul Hasan <moiz.hasan@ext.beyond.one>
2026-06-16 23:55:59 +0500

    fix: compilation error

--- BrowserJet/AppFoundation/Licensing/BrowserLicenseBackgroundMonitor.swift
+++ BrowserJet/AppFoundation/Licensing/BrowserLicenseBackgroundMonitor.swift
@@ -86,8 +86,7 @@ enum BrowserLicenseBackgroundMonitor {
         let delaySeconds = Double(delayNanoseconds) / 1_000_000_000
         var terminated = false
 
-        var autoQuitTimer: Timer?
-        autoQuitTimer = Timer(timeInterval: delaySeconds, repeats: false) { _ in
+        let autoQuitTimer = Timer(timeInterval: delaySeconds, repeats: false) { _ in
             NSApp.stopModal(withCode: .alertFirstButtonReturn)
             DispatchQueue.main.async {
                 guard !terminated else { return }
@@ -95,14 +94,12 @@ enum BrowserLicenseBackgroundMonitor {
                 QuitConfirmationController.terminateWithoutConfirmation()
             }
         }
-        guard let autoQuitTimer else { return }
         RunLoop.main.add(autoQuitTimer, forMode: .modalPanel)
         RunLoop.main.add(autoQuitTimer, forMode: .default)
 
         alert.runModal()
 
-        autoQuitTimer?.invalidate()
-        autoQuitTimer = nil
+        autoQuitTimer.invalidate()
         guard !terminated else { return }
         terminated = true
         QuitConfirmationController.terminateWithoutConfirmation()
